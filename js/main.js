a59c31e9aa6b85a9fbe481e4e6ea5c3a291319fb
The Verse <floatbeta@gmail.com>
2021-12-02 11:35:56 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop: false,
+	    autoplay: false,
+	    margin:0,
+	    animateOut: 'slideRight',
+	    animateIn: 'slideLeft',
+	    nav:false,
+	    dots: false,
+	    autoplayHoverPause: false,
+	    items: 5,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
